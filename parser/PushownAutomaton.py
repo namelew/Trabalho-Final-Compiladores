2f94797cec341b488e5e32ae9b8496177f203b29
namelew <diogomaciel.cunha@gmail.com>
2023-06-16 16:14:14 -0300

    Criando classe abstrata para representar as estruturas do Parser

--- parser/PushownAutomaton.py
+++ parser/PushownAutomaton.py
@@ -1,3 +1,25 @@
-class LR:
+from abc import abstractclassmethod
+from utils.SimbolTable import SimbolTable
+from parser.Table import Table
+
+class PushdownAutomaton:
+    def __init__(self, table:Table, tape:list[str], simbolTable:SimbolTable) -> None:
+        self.table = table
+        self.tape = tape
+        self.simbolTable = simbolTable
+    @abstractclassmethod
+    def Recognize(self) -> SimbolTable:
+        pass
+    @abstractclassmethod
+    def jump(self):
+        pass
+    @abstractclassmethod
+    def enqueue(self):
+        pass
+    @abstractclassmethod
+    def reduce(self):
+        pass
+
+class LR(PushdownAutomaton):
     def __init__(self) -> None:
         pass
--- parser/Table.py
+++ parser/Table.py
@@ -1,3 +1,15 @@
-class LR:
+from abc import abstractclassmethod
+
+class Table:
+    def __init__(self) -> None:
+        pass
+    @abstractclassmethod
+    def Build(self):
+        pass
+    @abstractclassmethod
+    def Action(self, row:str|int, collumn:str) -> str:
+        pass
+
+class LR(Table):
     def __init__(self) -> None:
         pass
