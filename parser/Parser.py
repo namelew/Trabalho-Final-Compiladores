ae9e85cf6c201478f185c4e7603f06b2f490ab7e
namelew <diogomaciel.cunha@gmail.com>
2023-07-05 11:21:06 -0300

    alterando leitura da parser table

--- parser/Parser.py
+++ parser/Parser.py
@@ -4,5 +4,5 @@ from parser.Table import LRTable
 
 class Parser:
     def __init__(self, tape:list[str], simbolTable:SimbolTable) -> None:
-        self.tablefile = "./input/parserTable.xml"
+        self.tablefile = "./input/parser_table.xml"
         self.automato:PushdownAutomaton = LR(LRTable(self.tablefile), tape, simbolTable)
--- parser/Table.py
+++ parser/Table.py
@@ -1,7 +1,10 @@
 from abc import abstractclassmethod
+import xml.etree.ElementTree as ET
 
 class Table:
     def __init__(self) -> None:
+        self.simbols:dict[list[str,int]] =  dict()
+        self.productions:dict[list[int,int]] =  dict()
         pass
     @abstractclassmethod
     def Build(self):
@@ -13,3 +16,13 @@ class Table:
 class LRTable(Table):
     def __init__(self, sourcefile:str) -> None:
         self.tablefile:str = sourcefile
+    def Build(self):
+        tree = ET.parse(self.tablefile)
+        for node in tree.findall('.//m_Symbol/Symbol'):
+            self.simbols[int(node.attrib['Index'])] = [node.attrib['Name'], int(node.attrib['Type'])]
+
+        for node in tree.findall('.//m_Production/Production'):
+            self.productions[int(node.attrib['Index'])] = [int(node.attrib['NonTerminalIndex']), int(node.attrib['SymbolCount'])]
+        
+        print(self.simbols)
+        print(self.productions)
